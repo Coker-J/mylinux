6322a6282bc58992887b0c4398614688c7783cad
Coker-J <117033815+Coker-J@users.noreply.github.com>
2022-11-10 17:52:30 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -2,9 +2,11 @@
 timedatectl set-timezone Asia/Shanghai
 sed -i 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/' /etc/gai.conf  
 apt update -y; apt install curl -y
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+
 apt install socat;curl https://get.acme.sh | sh -s email=12345@qq.com
 source ~/.bashrc
 read -p "输入你的域名：" domain
 acme.sh --issue --standalone -d $domain
-bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+
 
