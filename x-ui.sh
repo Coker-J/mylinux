923995eeacff9ac45243ecba12dac642639cc938
Coker-J <117033815+Coker-J@users.noreply.github.com>
2022-11-22 10:11:47 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -1,12 +1,13 @@
+read -p "输入你的域名：" domain
 
 timedatectl set-timezone Asia/Shanghai
 sed -i 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/' /etc/gai.conf  
 apt update -y; apt install curl -y; apt install iptables -y
-bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+#bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh) #原版x-ui
+bash <(curl -Ls https://raw.githubusercontent.com/FranzKafkaYu/x-ui/master/install.sh) #FranzKafkaYu版x-ui
 
 apt install socat;curl https://get.acme.sh | sh -s email=12345@qq.com
 source ~/.bashrc
-read -p "输入你的域名：" domain
 acme.sh --issue --standalone -d $domain
 
 
